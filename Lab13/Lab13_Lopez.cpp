7ef804436002b89acff22adaa1605627a73a4311
cl840965-collab <cl840965@gmail.com>
2026-06-13 01:16:33 +0000

    Lab 13

--- Lab13/Lab13_Lopez.cpp
+++ Lab13/Lab13_Lopez.cpp
@@ -17,5 +17,31 @@ int main(){
     greeting("Peter");
     string user = "Annie";
     greeting(user);
+
+    cout<<"\n---example 3: functions that return a value----"<<endl;
+    int x = dbnumber(6);
+    cout<<x<<endl;
+
+    cout<<"\n---example 4: function that returns the area of a rectangle"<<endl;
+    float a = arearectangle(2.5, 2);
+    cout<<"Area rectangle - "<<a<<endl;
+
+    cout<<"\n---example 5: function with alternative return value"<<endl;
+    string r = checknumber(0);
+    cout<<r<<endl;
+
+    cout<<"\n---example 6: composition of functions"<<endl;
+    int side = positivenumber();
+    int asquare = areasquare(side);
+    printresult(asquare);
+
+    cout<<"\n---example 7: function calling function"<<endl;
+    cout<<tripleaddition();
+
+    cout<<"\n---EXCERCISE"<<endl;
+    cout<<collectone()<<endl;
+    int stuff = collectone();
+    collectfifteen(stuff);
+
     return 0;
 }
--- Lab13/Lab13_function_Lopez.cpp
+++ Lab13/Lab13_function_Lopez.cpp
@@ -22,3 +22,83 @@ void printhi(){
 void greeting(string username){
     cout<<"Good morning"<<username<<endl;
 }
+
+
+// example 3: function that returns a value
+// returns the double of a number(integer)
+// the number is passed to the function as arugments
+int dbnumber(int n){ 
+    int dbn = n*2;
+    return dbn;
+}
+
+
+// example 4: function that returns the area of a rectangle
+// returning value is float, area rectangle = width * length
+float arearectangle(int width, int length){
+    return width * length;
+    float n = 2*(width * length);
+    return n;
+}
+
+// example 5: function that checks if a is positive, negative, or zero
+string checknumber(int number){
+    if(number>0)
+        return "positive";
+    else if (number < 0)
+        return "negative";
+    else
+        return "zero";
+}
+
+// example 6: composition of functions
+// function 1: collect and return a positive
+// validate that the number is positive before returning it
+int positivenumber(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    // recollect n if n is not a positive number
+    while(n<=0){
+        cout<<"Error! Enter a positive number: ";
+        cin>>n;
+    }
+    return n;
+}
+
+// function that calculates the area of the square
+int areasquare(int side){
+    return side*side;
+}
+
+void printresult(int area){
+    cout<<"The are of a square is "<<area<<endl;
+}
+
+// example 7: function calling function
+// function 1: returns the sum of two numbers
+int addition(int a, int b){
+    return a+b;
+}
+
+// function 2: returns the triple of the sum of two numbers
+int tripleaddition(){
+    return 3*(addition(2,3)); // function calling functions
+}
+
+int collectone(){
+    int o;
+    cout<<"Enter a number 1-10: ";
+    cin>>o;
+    while(o<=1 || o>=10){
+        cout<<"Enter a number 1 through 10: ";
+        cin>>o;
+    }
+    return o;
+}
+
+void collectfifteen(int start){
+    for(int i = start; i<=15; i++){
+        cout<<i<<endl;
+    }
+}
